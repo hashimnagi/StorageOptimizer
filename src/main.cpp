fc777aba64659f673cb3098fd912be28f246a5d5
hashimnagi <hashimnagi77@gmail.com>
2026-07-25 22:48:32 +0500

    Modified the main design

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "Application.h"
+#include <iostream>
 
 int main() {
+    std::cerr << "MAIN STARTED\n";
+
     Application app;
+
+    std::cerr << "APPLICATION CREATED\n";
+
     app.run();
+
+    std::cerr << "APPLICATION FINISHED\n";
+
     return 0;
 }
